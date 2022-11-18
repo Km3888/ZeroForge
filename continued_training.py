361c9b7a9977de2589b2d14adc5367f15e48d341
km3888 <km3888@nyu.edu>
2022-11-18 15:33:48 -0500

    changed hard loss tracking and paralleled resizer

--- continued_training.py
+++ continued_training.py
@@ -30,7 +30,7 @@ import PIL
 writer = SummaryWriter()
 
 def clip_loss(query,args,clip_model,autoencoder,latent_flow_model,renderer,rotation,resizer,iter):
-    text_emb,ims,hard_ims =generate_on_train_query(args,clip_model,autoencoder,latent_flow_model,renderer,query,args.batch_size,rotation,resizer,iter)
+    text_emb,ims = generate_on_train_query(args,clip_model,autoencoder,latent_flow_model,renderer,query,args.batch_size,rotation,resizer,iter)
     
     im_embs=clip_model.encode_image(ims)
     losses=-1*torch.cosine_similarity(text_emb,im_embs)
@@ -38,12 +38,8 @@ def clip_loss(query,args,clip_model,autoencoder,latent_flow_model,renderer,rotat
     
     im_samples = [ims[0],ims[1],ims[2]]
     im_samples = [im.detach().cpu() for im in im_samples]
-    
-    hard_im_embs=clip_model.encode_image(hard_ims)
-    hard_losses = -1*torch.cosine_similarity(text_emb,hard_im_embs)
-    hard_loss = hard_losses.mean()
-    
-    return losses.mean(), hard_loss, im_samples
+        
+    return losses.mean(), im_samples
 
 def get_clip_model(args):
     if args.clip_model_type == "B-16":
@@ -89,7 +85,7 @@ def generate_on_train_query(args,clip_model,autoencoder,latent_flow_model,render
 
     out_3d = autoencoder.decoding(decoder_embs, query_points).view(batch_size, voxel_size, voxel_size, voxel_size).to(args.device)
     out_3d_soft = torch.sigmoid(100*(out_3d-args.threshold))
-    
+        
     if not iter%10:
         out_3d_hard = out_3d.detach() > args.threshold
         rgbs_hard = []
@@ -97,36 +93,34 @@ def generate_on_train_query(args,clip_model,autoencoder,latent_flow_model,render
             rand_rotate_i = transform.rotate_random(out_3d_soft[i].float().unsqueeze(0).unsqueeze(0))
             rand_rotate_i_hard = transform.rotate_random(out_3d_hard[i].float().unsqueeze(0).unsqueeze(0))
             for axis in range(3):
-                out_2d_i_hard = renderer.render(volume=rand_rotate_i_hard.squeeze(),axis=axis).double()
-                
-                rgb_i_hard = resize_transform(out_2d_i_hard.unsqueeze(0).unsqueeze(0).expand(-1,3,-1,-1))
-                
-                rgbs_hard.append(rgb_i_hard)
+                out_2d_i_hard = renderer.render(volume=rand_rotate_i_hard.squeeze(),axis=axis).double()                
+                rgbs_hard.append(out_2d_i_hard.unsqueeze(0))
     
         rgbs_hard = torch.cat(rgbs_hard,0)
+        rgbs_hard = rgbs_hard.unsqueeze(1).expand(-1,3,-1,-1)
+        rgbs_hard = resizer(rgbs_hard)
+        
         hard_im_embeddings = clip_model.encode_image(rgbs_hard)
-        hard_loss = -1*torch.cosine_similarity(text_features,hard_im_embeddings)
+        hard_loss = -1*torch.cosine_similarity(text_features,hard_im_embeddings).mean()
         #write to tensorboard
         writer.add_scalar('Loss/hard_loss', hard_loss, iter)
         voxel_render_loss = -1* evaluate_true_voxel(out_3d_hard[0],args,clip_model,text_features,iter,text_in)
         writer.add_scalar('Loss/voxel_render_loss', voxel_render_loss, iter)
         
     # out_3d_soft = out_3d_soft.unsqueeze(0)
-    resize_transform = resizer
+    
     rgbs = []
     for i in range(batch_size):
         rand_rotate_i = transform.rotate_random(out_3d_soft[i].float().unsqueeze(0).unsqueeze(0))
         for axis in range(3):
-            out_2d_i = renderer.render(volume=rand_rotate_i.squeeze(),axis=axis).double()
-            
-            rgb_i = resize_transform(out_2d_i.unsqueeze(0).unsqueeze(0).expand(-1,3,-1,-1))
-            
-            rgbs.append(rgb_i)
+            out_2d_i = renderer.render(volume=rand_rotate_i.squeeze(),axis=axis).double()            
+            rgbs.append(out_2d_i.unsqueeze(0))
     
     rgbs = torch.cat(rgbs,0)
+    rgbs = rgbs.unsqueeze(1).expand(-1,3,-1,-1)
+    rgbs =  resizer(rgbs)
     
     # clip_embs = clip_model.encode_image(rgbs)
-            
     # save_image(out_2d_1, "rotated_car_out_2d_1.png")
     
     return text_features,rgbs
@@ -191,7 +185,7 @@ def test_train(args,clip_model,autoencoder,latent_flow_model,renderer):
         flow_optimizer.zero_grad()
         net_optimizer.zero_grad()
         
-        loss, hard_loss, rgb_images =clip_loss(sample_query,args,clip_model,autoencoder,latent_flow_model,renderer,rotation,resizer,iter)
+        loss, rgb_images =clip_loss(sample_query,args,clip_model,autoencoder,latent_flow_model,renderer,rotation,resizer,iter)
         #save image
         grid = torchvision.utils.make_grid(rgb_images)
         writer.add_image('rendered image', grid, iter)
@@ -201,7 +195,6 @@ def test_train(args,clip_model,autoencoder,latent_flow_model,renderer):
         losses.append(loss.item())
         
         writer.add_scalar('Loss/train', loss.item(), iter)
-        writer.add_scalar('Loss/hard', hard_loss.item(), iter)
         
         flow_optimizer.step()
         net_optimizer.step()
