05e5c6a33daec6475eea679283447a63d23ed1ec
km3888 <km3888@nyu.edu>
2022-11-21 12:46:49 -0500

    updated training script

--- continued_training.sh
+++ continued_training.sh
@@ -3,4 +3,4 @@
 eval "$(conda shell.bash hook)"
 conda activate clip_forge
 
-python continued_training.py --beta 25.0 --text_query "a knife" --checkpoint_dir_base ./exps/models/autoencoder --checkpoint best_iou --checkpoint_dir_prior ./exps/models/prior/ --checkpoint_nf best
+python continued_training.py --gpu 0 --beta 25.0 --text_query "a fork" --checkpoint_dir_base ./exps/models/autoencoder --checkpoint best_iou --checkpoint_dir_prior ./exps/models/prior/ --checkpoint_nf best
