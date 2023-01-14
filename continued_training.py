changed devices and fixed scaling bug

continued_training.py
@@ -26,19 +26,25 @@ import torchvision.transforms as T
 from torch.utils.tensorboard import SummaryWriter
 
 import PIL
-
-
+import sys
+import numpy as np
 
 def clip_loss(args,query_array,clip_model,autoencoder,latent_flow_model,renderer,resizer,iter,text_features=None):
     # text_emb,ims = generate_single_query(args,clip_model,autoencoder,latent_flow_model,renderer,query,args.batch_size,rotation,resizer,iter)
     
     text_embs,ims = generate_for_query_array(args,clip_model,autoencoder,latent_flow_model,renderer,query_array,resizer,iter,text_features=text_features)
-    im= im.to('cuda:0')
+    ims= ims.to(args.device)
     im_embs=clip_model.encode_image(ims)
     text_embs=text_embs.unsqueeze(1).expand(-1,3,-1).reshape(-1,512)
     losses=-1*torch.cosine_similarity(text_embs,im_embs)
     loss = losses.mean()
     
+    import numpy as np
+    if iter<50:
+        im_np = ims.cpu().detach().numpy()
+        with open('out_ims/out_ims_%s.npy' % iter,'wb') as f:
+            np.save(f,im_np)
+
     if args.use_tensorboard and not iter%10:
         im_samples= ims.view(-1,3,224,224)
         grid = torchvision.utils.make_grid(im_samples, nrow=3)
@@ -107,16 +113,16 @@ def generate_for_query_array(args,clip_model,autoencoder,latent_flow_model,rende
     
     noise = torch.Tensor(batch_size, args.emb_dims).normal_().to(args.device)
     decoder_embs = latent_flow_model.sample(batch_size, noise=noise, cond_inputs=text_features)
-    
+
     out_3d = autoencoder.decoding(decoder_embs, query_points).view(batch_size, voxel_size, voxel_size, voxel_size).to(args.device)
     out_3d_soft = torch.sigmoid(args.beta*(out_3d-args.threshold)).clone()
-   
+    
     if not iter%50:
         out_3d_hard = out_3d.detach() > args.threshold
         #Currently only doing all 3 angles for ea, could try something similar
         #for nvr+ once I understand the camera angle better
         #renderer expects [batch,voxel_size,voxel_size,voxel_size]    
-        rgbs_hard = renderer.render(out_3d_hard.float()).double().to('cuda:0')
+        rgbs_hard = renderer.render(out_3d_hard.float()).double().to(args.device)
         rgbs_hard = resizer(rgbs_hard)
         
         hard_im_embeddings = clip_model.encode_image(rgbs_hard)
@@ -130,7 +136,9 @@ def generate_for_query_array(args,clip_model,autoencoder,latent_flow_model,rende
             writer.add_scalar('Loss/voxel_render_loss', voxel_render_loss, iter)
     
     #REFACTOR put all these into a single method which works for hard or soft
-    rgbs = renderer.render(out_3d_soft).double()            
+    rgbs_float = renderer.render(out_3d_soft).double()
+
+    rgbs = rgbs_float.double()
     rgbs = resizer(rgbs)
         
     return text_features,rgbs
@@ -150,7 +158,8 @@ def evaluate_true_voxel(out_3d,args,clip_model,text_features,i):
     
     voxel_ims = torch.cat(voxel_ims,0)
     grid = torchvision.utils.make_grid(voxel_ims, nrow=num_shapes)
-    
+        
+
     if args.use_tensorboard:
         writer.add_image('voxel image', grid, i)
     # #convert to 224x224 image with 3 channels
@@ -241,6 +250,9 @@ def main(args):
     
     latent_flow_network = latent_flows.get_generator(args.emb_dims, args.cond_emb_dim, device, flow_type=args.flow_type, num_blocks=args.num_blocks, num_hidden=args.num_hidden)
     if not args.uninitialized:
+        print(args.checkpoint_dir_prior)
+        print(args.checkpoint)
+        sys.stdout.flush()
         checkpoint_nf_path = os.path.join(args.checkpoint_dir_prior,  args.checkpoint_nf +".pt")
         checkpoint = torch.load(checkpoint_nf_path, map_location=args.device)
         latent_flow_network.load_state_dict(checkpoint['model'])

rendering/nvr_layer.py
@@ -56,7 +56,7 @@ def estimate_ground_image(object_voxels):
     ground_voxel_color = np.concatenate([ground_voxel_color, ground_occupancy],
                                         axis=-1)
 
-    print('ground_voxel_color:',ground_voxel_color.shape)
+    #print('ground_voxel_color:',ground_voxel_color.shape)
 
     scene_voxels = object_voxels*(1-ground_occupancy) + \
                     ground_voxel_color*ground_occupancy
@@ -73,7 +73,7 @@ def estimate_ground_image(object_voxels):
                                                 euler_angles_x,
                                                 euler_angles_y,
                                                 translation_vector)
-    print('interpolated:',interpolated_voxels.shape)
+    #print('interpolated:',interpolated_voxels.shape)
 
     color_input, alpha_input = tf.split(interpolated_voxels, [3, 1], axis=-1)
     voxel_img = visual_hull.render(color_input*alpha_input)

rendering/nvr_server.py
@@ -18,11 +18,12 @@ GROUND_COLOR = np.array((136., 162, 199))/255.
 BLENDER_SCALE = 2
 DIAMETER = 4.2  # The voxel area in world coordinates
 
+device = 'cuda:1'
 
 latest_checkpoint = '/scratch/km3888/nvr_weights/checkpoints/model.ckpt-126650'
 tf.compat.v1.reset_default_graph()
 g = tf.compat.v1.Graph()
-with tf.device('/device:GPU:1'):
+with tf.device('/device:GPU:0'):
     with g.as_default():
         vol_placeholder = tf.compat.v1.placeholder(tf.float32,
                                             shape=[None, VOXEL_SIZE, VOXEL_SIZE, VOXEL_SIZE, 4],
@@ -76,6 +77,7 @@ def render_tf_backward(final_composite,interpolated_voxels,upstream_gradient):
     d = upstream_gradient.cpu().numpy()
     with tf.compat.v1.Session(graph=g) as sess:
         saver.restore(sess, latest_checkpoint)
+        devices = sess.list_devices()
         feed_dict = {vol_placeholder: a,
                     rerender_placeholder: b,
                     light_placeholder: c,
@@ -93,19 +95,13 @@ class NVR(torch.autograd.Function):
         predictions = render_tf_forward(final_composite,interpolated_voxels)
         #gives nice output if you use matplotlib.pyplot
         torch_predictions= torch.from_numpy(predictions).float()
-        print('input sizes:')
-        print(final_composite.shape)
-        print(interpolated_voxels.shape)
         return torch_predictions
     
     @staticmethod
     def backward(ctx, grad_output):
         final_composite,interpolated_voxels = ctx.saved_tensors
         d_composite,d_interpolated = render_tf_backward(final_composite,interpolated_voxels,grad_output)
-        print('backward sizes:')
-        print(d_composite.shape)
-        print(d_interpolated.shape)
-        return torch.from_numpy(d_composite).to('cuda:0'),torch.from_numpy(d_interpolated).to('cuda:0')
+        return torch.from_numpy(d_composite).to(device),torch.from_numpy(d_interpolated).to(device)
 
 
 class NVR_Renderer:
@@ -120,13 +116,13 @@ class NVR_Renderer:
         final_composite = final_composite.permute(0,2,3,1)
         output=NVR.apply(final_composite,interpolated_voxels)
         permuted_output=output.permute(0,3,1,2)
-        return permuted_output
+        return permuted_output*0.5+0.5
 
 def test_nvr():
     path="airplane_128.npy"
     with open(path, 'rb') as f:
         voxel = np.load(f)
-    voxel = torch.from_numpy(voxel).float().to('cuda:0')
+    voxel = torch.from_numpy(voxel).float().to(device)
     voxel.requires_grad=True
     
     renderer = NVR_Renderer()
@@ -136,7 +132,7 @@ def test_nvr():
     path="airplane_128.npy"
     with open(path, 'rb') as f:
         new_voxel = np.load(f)
-    new_voxel = torch.from_numpy(new_voxel).float().to('cuda:0')
+    new_voxel = torch.from_numpy(new_voxel).float().to(device)
     new_voxel.requires_grad=True
     
     new_voxel = torch.stack([voxel,new_voxel],dim=0)
@@ -146,7 +142,7 @@ if __name__=="__main__":
     path="airplane_128.npy"
     with open(path, 'rb') as f:
         voxel = np.load(f)
-    voxel = torch.from_numpy(voxel).float().to('cuda:0')
+    voxel = torch.from_numpy(voxel).float().to(device)
     voxel.requires_grad=True
     
     renderer = NVR_Renderer()
@@ -156,7 +152,7 @@ if __name__=="__main__":
     path="airplane_128.npy"
     with open(path, 'rb') as f:
         new_voxel = np.load(f)
-    new_voxel = torch.from_numpy(new_voxel).float().to('cuda:0')
+    new_voxel = torch.from_numpy(new_voxel).float().to(device)
     new_voxel.requires_grad=True
     
     new_voxel = torch.stack([voxel,new_voxel],dim=0)