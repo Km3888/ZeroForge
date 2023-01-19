243f014ddabc28b8761855ebab6408d216d32441
km3888 <km3888@nyu.edu>
2023-01-18 19:58:42 -0500

    removed unnecessary files

--- continued_training.sh
+++
@@ -1,5 +0,0 @@
-
-python continued_training.py --num_voxels 128 --gpu 0 --beta 50.0 --query_array 
-"fork" --num_views 4 --checkpoint_dir_base ./exps/models/autoencoder --checkpoint 
-best_iou --checkpoint_dir_prior ./exps/models/prior/ --checkpoint_nf best --renderer 
-nvr+
