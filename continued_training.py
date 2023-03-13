58527bfba8ea7fb43f33ce3d0986f491b2ebd836
km3888 <km3888@nyu.edu>
2023-03-13 12:29:19 -0400

    refactored continued_training

--- continued_training.py
+++ continued_training.py
@@ -31,19 +31,15 @@ import sys
 import numpy as np
 import torch.nn as nn
 
-def get_type(visual_model):
-    return visual_model.conv1.weight.dtype
+from continued_utils import query_arrays, make_writer, get_networks, get_local_parser, get_clip_model,get_text_embeddings,get_type
 
 def clip_loss(args,query_array,visual_model,autoencoder,latent_flow_model,renderer,resizer,iter,text_features):
-    # text_emb,ims = generate_single_query(args,clip_model,autoencoder,latent_flow_model,renderer,query,args.batch_size,rotation,resizer,iter)
     out_3d = gen_shapes(query_array,args,autoencoder,latent_flow_model,text_features)
     out_3d_soft = torch.sigmoid(args.beta*(out_3d-args.threshold))#.clone()
     
-    #REFACTOR put all these into a single method which works for hard or soft
     ims = renderer.render(out_3d_soft,orthogonal=args.orthogonal).double()
     ims = resizer(ims)
 
-    # im_embs=clip_model.encode_image(ims)
     im_embs=visual_model(ims.type(visual_model_type))
     if args.renderer=='ea':
         #baseline renderer gives 3 dimensions
@@ -58,45 +54,6 @@ def clip_loss(args,query_array,visual_model,autoencoder,latent_flow_model,render
         args.writer.add_image('images', grid, iter)
 
     return loss
-
-def get_clip_model(args):
-    if args.clip_model_type == "B-16":
-        print("Bigger model is being used B-16")
-        clip_model, clip_preprocess = clip.load("ViT-B/16", device=args.device)
-        cond_emb_dim = 512
-    elif args.clip_model_type == "RN50x16":
-        print("Using the RN50x16 model")
-        clip_model, clip_preprocess = clip.load("RN50x16", device=args.device)
-        cond_emb_dim = 768
-    else:
-        clip_model, clip_preprocess = clip.load("ViT-B/32", device=args.device)
-        cond_emb_dim = 512
-    
-    input_resolution = clip_model.visual.input_resolution
-    #train_cond_embs_length = clip_model.train_cond_embs_length
-    vocab_size = clip_model.vocab_size
-    #cond_emb_dim  = clip_model.embed_dim
-    #print("Model parameters:", f"{np.sum([int(np.prod(p.shape)) for p in clip_model.parameters()]):,}")
-    print("cond_emb_dim:", cond_emb_dim)
-    print("Input resolution:", input_resolution)
-    #print("train_cond_embs length:", train_cond_embs_length)
-    print("Vocab size:", vocab_size)
-    args.n_px = input_resolution
-    args.cond_emb_dim = cond_emb_dim
-    return args, clip_model
-
-def get_text_embeddings(args,clip_model,query_array):
-    # get the text embedding for each query
-    text_tokens = []
-    with torch.no_grad():
-        for text in query_array:
-            text_tokens.append(clip.tokenize([text]).detach().to(args.device))
-        
-    text_tokens = torch.cat(text_tokens,dim=0)
-    text_features = clip_model.encode_text(text_tokens)
-    text_features = text_features / text_features.norm(dim=-1, keepdim=True)
-
-    return text_features
 
 #TODO remove query_array param
 def gen_shapes(query_array,args,autoencoder,latent_flow_model,text_features):
@@ -122,7 +79,8 @@ def do_eval(renderer,query_array,args,visual_model,autoencoder,latent_flow_model
     # with open(f'out_3d/{args.learning_rate}_{args.query_array}/out_3d_{iter}.npy', 'wb') as f:
     #     np.save(f, out_3d.cpu().detach().numpy())
     out_3d_hard = out_3d.detach() > args.threshold
-    rgbs_hard = renderer.render(out_3d_hard.float(),orthogonal=args.orthogonal).double().to(args.device)
+    # rgbs_hard = renderer.render(out_3d_hard.float(),orthogonal=args.orthogonal).double().to(args.device)
+    rgbs_hard = renderer.render(out_3d_hard.float(),orthogonal=args.orthogonal).to(args.device)
     rgbs_hard = resizer(rgbs_hard)
     # hard_im_embeddings = clip_model.encode_image(rgbs_hard)
     hard_im_embeddings = visual_model(rgbs_hard.type(visual_model_type))
@@ -141,7 +99,8 @@ def evaluate_true_voxel(out_3d_hard,args,visual_model,text_features,i):
     # code for saving the "true" voxel image
     voxel_ims=[]
     num_shapes = out_3d_hard.shape[0]
-    #num_shapes = min(num_shapes, 3)
+    if len(set(args.query_array))==1:
+        num_shapes = min(num_shapes, 3)
     for shape in range(num_shapes):
         save_path = '/scratch/km3888/queries/%s/sample_%s_%s.png' % (args.writer.log_dir[5:],i,shape)
         voxel_save(out_3d_hard[shape].squeeze().detach().cpu(), None, out_file=save_path)
@@ -162,45 +121,9 @@ def evaluate_true_voxel(out_3d_hard,args,visual_model,text_features,i):
     # #convert to 224x224 image with 3 channels
     voxel_tensor = T.Resize((224,224))(voxel_ims)
     # get CLIP embedding
-    # voxel_image_embedding = clip_model.encode_image(voxel_tensor.to(args.device))
     voxel_image_embedding = visual_model(voxel_tensor.to(args.device).type(visual_model_type))
     voxel_similarity = torch.cosine_similarity(text_features, voxel_image_embedding).mean()
     return voxel_similarity
-
-
-def get_local_parser(mode="args"):
-    parser = parsing(mode="parser")
-    parser.add_argument("--num_blocks", type=int, default=5, help='Num of blocks for prior')
-    parser.add_argument("--flow_type", type=str, default='realnvp_half', help='flow type: mf, glow, realnvp ')
-    parser.add_argument("--num_hidden", type=int, default=1024, help='Number of parameter for flow model')
-    parser.add_argument("--latent_load_checkpoint", type=str, default=None, help='Checkpoint to load latent flow model')
-    parser.add_argument("--num_views",  type=int, default=5, metavar='N', help='Number of views')
-    parser.add_argument("--clip_model_type",  type=str, default='B-32', metavar='N', help='what model to use')
-    parser.add_argument("--noise",  type=str, default='add', metavar='N', help='add or remove')
-    parser.add_argument("--seed_nf",  type=int, default=1, metavar='N', help='add or remove')
-    parser.add_argument("--images_type",  type=str, default=None, help='img_choy13 or img_custom')
-    parser.add_argument("--n_px",  type=int, default=224, help='Resolution of the image')
-    parser.add_argument("--checkpoint_dir_base", type=str, default=None)
-    parser.add_argument("--checkpoint_dir_prior", type=str, default=None)
-    parser.add_argument("--checkpoint_nf",  type=str, default="best", help='what is the checkpoint for nf')
-    parser.add_argument("--text_query",  type=str, default="")
-    parser.add_argument("--beta",  type=float, default=75, help='regularization coefficient')
-    parser.add_argument("--learning_rate",  type=float, default=01e-06, help='learning rate') #careful, base parser has "lr" param with different default value
-    parser.add_argument("--use_tensorboard",  type=bool, default=True, help='use tensorboard')
-    parser.add_argument("--query_array",  type=str, default=None, help='multiple queries') 
-    parser.add_argument("--uninitialized",  type=bool, default=False, help='Use untrained networks')
-    parser.add_argument("--num_voxels",  type=int, default=32, help='number of voxels')
-    # parser.add_argument("--threshold",  type=float, default=0.5, help='threshold for voxelization')
-    parser.add_argument("--switch_point",type=float, default=None, help='switch point for the renderer')
-    parser.add_argument("--renderer",  type=str, default='ea')
-    parser.add_argument("--orthogonal",  type=bool, default=False, help='use orthogonal views')
-    parser.add_argument("--setting", type=int, default=None)
-    parser.add_argument("--slurm_id", type=int, default=None)
-    if mode == "args":
-        args = parser.parse_args()
-        return args
-    else:
-        return parser
 
 def test_train(args,clip_model,autoencoder,latent_flow_model,renderer):    
     resizer = T.Resize(224)
@@ -208,7 +131,6 @@ def test_train(args,clip_model,autoencoder,latent_flow_model,renderer):
     net_optimizer=optim.Adam(autoencoder.parameters(), lr=args.learning_rate)
 
     losses = []
-    
     if args.query_array in query_arrays:
         query_array = query_arrays[args.query_array]
     else:
@@ -242,24 +164,21 @@ def test_train(args,clip_model,autoencoder,latent_flow_model,renderer):
             renderer.model.to(args.device)
 
         if not iter%300:
-            with torch.autocast('cuda'):
+            with torch.cuda.amp.autocast():
                 do_eval(renderer,query_array,args,visual_model,autoencoder,latent_flow_model,resizer,iter,text_features)
-                    
         
         if not (iter%5000) and iter!=0:
             #save encoder and latent flow network
             torch.save(latent_flow_model.state_dict(), '/scratch/km3888/queries/%s/flow_model_%s.pt' % (args.writer.log_dir[5:],iter))
             torch.save(autoencoder.module.encoder.state_dict(), '/scratch/km3888/queries/%s/aencoder_%s.pt' % (args.writer.log_dir[5:],iter))
             
-            
         flow_optimizer.zero_grad()
         net_optimizer.zero_grad()
         
-        with torch.autocast('cuda'):
+        with torch.cuda.amp.autocast():
             loss = clip_loss(args,query_array,visual_model,autoencoder,latent_flow_model,renderer,resizer,iter,text_features)        
         
         loss.backward()
-                
         losses.append(loss.item())
         
         if args.use_tensorboard:
@@ -276,50 +195,10 @@ def test_train(args,clip_model,autoencoder,latent_flow_model,renderer):
     
     print(losses)
 
-def make_writer(args):
-    if not args.use_tensorboard:
-        return None
-    tensorboard_comment = '_%s_lr=%s_beta=%s_gpu=%s_baseline=%s_v=%s_k=%s_r=%s'% (args.query_array,args.learning_rate,args.beta,args.gpu[0],args.uninitialized,args.num_voxels,args.num_views,args.renderer)
-    if args.switch_point is not None:
-        tensorboard_comment += '_s=%s' % args.switch_point
-    if args.orthogonal:
-        tensorboard_comment += '_orthogonal'
-    if args.slurm_id is not None:
-        tensorboard_comment += str(args.slurm_id)
-    tensorboard_comment += 'amp'
-    assert args.renderer in ['ea','nvr+']
-    return SummaryWriter(comment=tensorboard_comment)
 
 def main(args):
-    args.writer=make_writer(args)
-        
-    # if not os.path.exists(f'out_3d/{args.learning_rate}_{args.query_array}'):
-    #     os.mkdir(f'out_3d/{args.learning_rate}_{args.query_array}')
+    args.writer = make_writer(args)
 
-def get_networks(args):
-    net = autoencoder.EncoderWrapper(args).to(args.device)    
-    latent_flow_network = latent_flows.get_generator(args.emb_dims, args.cond_emb_dim, args.device, flow_type=args.flow_type, num_blocks=args.num_blocks, num_hidden=args.num_hidden)
-    if not args.uninitialized:
-        print(args.checkpoint_dir_prior)
-        print(args.checkpoint)
-        sys.stdout.flush()
-        checkpoint_nf_path = os.path.join(args.checkpoint_dir_prior,  args.checkpoint_nf +".pt")
-        checkpoint = torch.load(checkpoint_nf_path, map_location=args.device)
-        latent_flow_network.load_state_dict(checkpoint['model'])
-        checkpoint = torch.load(args.checkpoint_dir_base +"/"+ args.checkpoint +".pt", map_location=args.device)
-        net.load_state_dict(checkpoint['model'])
-        net.eval()
-    return net, latent_flow_network
-
-def main(args):
-    if args.use_tensorboard:
-        tensorboard_comment = '_%s_lr=%s_beta=%s_gpu=%s_baseline=%s_v=%s_k=%s_r=%s'% (args.query_array,args.learning_rate,args.beta,args.gpu[0],args.uninitialized,args.num_voxels,args.num_views,args.renderer)
-        if args.switch_point is not None:
-            tensorboard_comment += '_s=%s' % args.switch_point
-        if args.orthogonal:
-            tensorboard_comment += '_orthogonal'
-        args.writer=SummaryWriter(comment=tensorboard_comment)
-    assert args.renderer in ['ea','nvr+']
     device, gpu_array = helper.get_device(args)
     args.device = device
     
@@ -338,18 +217,6 @@ def main(args):
 
     test_train(args,clip_model,net,latent_flow_network,renderer)
     
-query_arrays = {
-                "wineglass": ["wineglass"],
-                "spoon": ["spoon"],
-                "fork": ["fork"],
-                "hammer": ["hammer"],
-                "four": ["spoon","fork","wineglass","knife","wineglass"],
-                "six": ['wineglass','spoon','fork','knife','screwdriver','hammer'],
-                "nine": ['wineglass','spoon','fork','knife','screwdriver','hammer',"soccer ball", "football","plate"],
-                "fourteen": ["wineglass','spoon','fork','knife','screwdriver','hammer","pencil","screw","plate","mushroom","umbrella","thimble","sombrero","sandal"]
-}
-#REFACTOR put query arrays in a separate file
-
 if __name__=="__main__":
     args=get_local_parser()
     print('renderer %s' % args.renderer)
---
+++ continued_utils.py
@@ -0,0 +1,126 @@
+import clip
+from torch.utils.tensorboard import SummaryWriter
+from networks import autoencoder, latent_flows
+from train_autoencoder import parsing
+import torch
+import sys
+import os
+import numpy as np
+
+
+query_arrays = {
+                "wineglass": ["wineglass"],
+                "spoon": ["spoon"],
+                "fork": ["fork"],
+                "hammer": ["hammer"],
+                "four": ["spoon","fork","wineglass","knife","wineglass"],
+                "six": ['wineglass','spoon','fork','knife','screwdriver','hammer'],
+                "nine": ['wineglass','spoon','fork','knife','screwdriver','hammer',"soccer ball", "football","plate"],
+                "fourteen": ["wineglass','spoon','fork','knife','screwdriver','hammer","pencil","screw","plate","mushroom","umbrella","thimble","sombrero","sandal"]
+}
+
+def make_writer(args):
+    if not args.use_tensorboard:
+        return None
+    tensorboard_comment = 'q=%s_lr=%s_beta=%s_gpu=%s_baseline=%s_v=%s_k=%s_r=%s'% (args.query_array,args.learning_rate,args.beta,args.gpu[0],args.uninitialized,args.num_voxels,args.num_views,args.renderer)
+    if args.switch_point is not None:
+        tensorboard_comment += '_s=%s' % args.switch_point
+    if args.orthogonal:
+        tensorboard_comment += '_orthogonal'
+    if args.slurm_id is not None:
+        tensorboard_comment += str(args.slurm_id)
+    tensorboard_comment += 'amp'
+    assert args.renderer in ['ea','nvr+']
+    return SummaryWriter(comment=tensorboard_comment)
+
+def get_networks(args):
+    net = autoencoder.EncoderWrapper(args).to(args.device)    
+    latent_flow_network = latent_flows.get_generator(args.emb_dims, args.cond_emb_dim, args.device, flow_type=args.flow_type, num_blocks=args.num_blocks, num_hidden=args.num_hidden)
+    if not args.uninitialized:
+        print(args.checkpoint_dir_prior)
+        print(args.checkpoint)
+        sys.stdout.flush()
+        checkpoint_nf_path = os.path.join(args.checkpoint_dir_prior,  args.checkpoint_nf +".pt")
+        checkpoint = torch.load(checkpoint_nf_path, map_location=args.device)
+        latent_flow_network.load_state_dict(checkpoint['model'])
+        checkpoint = torch.load(args.checkpoint_dir_base +"/"+ args.checkpoint +".pt", map_location=args.device)
+        net.load_state_dict(checkpoint['model'])
+        net.eval()
+    return net, latent_flow_network
+
+def get_local_parser(mode="args"):
+    parser = parsing(mode="parser")
+    parser.add_argument("--num_blocks", type=int, default=5, help='Num of blocks for prior')
+    parser.add_argument("--flow_type", type=str, default='realnvp_half', help='flow type: mf, glow, realnvp ')
+    parser.add_argument("--num_hidden", type=int, default=1024, help='Number of parameter for flow model')
+    parser.add_argument("--latent_load_checkpoint", type=str, default=None, help='Checkpoint to load latent flow model')
+    parser.add_argument("--num_views",  type=int, default=5, metavar='N', help='Number of views')
+    parser.add_argument("--clip_model_type",  type=str, default='B-32', metavar='N', help='what model to use')
+    parser.add_argument("--noise",  type=str, default='add', metavar='N', help='add or remove')
+    parser.add_argument("--seed_nf",  type=int, default=1, metavar='N', help='add or remove')
+    parser.add_argument("--images_type",  type=str, default=None, help='img_choy13 or img_custom')
+    parser.add_argument("--n_px",  type=int, default=224, help='Resolution of the image')
+    parser.add_argument("--checkpoint_dir_base", type=str, default=None)
+    parser.add_argument("--checkpoint_dir_prior", type=str, default=None)
+    parser.add_argument("--checkpoint_nf",  type=str, default="best", help='what is the checkpoint for nf')
+    parser.add_argument("--text_query",  type=str, default="")
+    parser.add_argument("--beta",  type=float, default=75, help='regularization coefficient')
+    parser.add_argument("--learning_rate",  type=float, default=01e-06, help='learning rate') #careful, base parser has "lr" param with different default value
+    parser.add_argument("--use_tensorboard",  type=bool, default=True, help='use tensorboard')
+    parser.add_argument("--query_array",  type=str, default=None, help='multiple queries') 
+    parser.add_argument("--uninitialized",  type=bool, default=False, help='Use untrained networks')
+    parser.add_argument("--num_voxels",  type=int, default=32, help='number of voxels')
+    # parser.add_argument("--threshold",  type=float, default=0.5, help='threshold for voxelization')
+    parser.add_argument("--switch_point",type=float, default=None, help='switch point for the renderer')
+    parser.add_argument("--renderer",  type=str, default='ea')
+    parser.add_argument("--orthogonal",  type=bool, default=False, help='use orthogonal views')
+    parser.add_argument("--setting", type=int, default=None)
+    parser.add_argument("--slurm_id", type=int, default=None)
+    if mode == "args":
+        args = parser.parse_args()
+        return args
+    else:
+        return parser
+
+def get_text_embeddings(args,clip_model,query_array):
+    # get the text embedding for each query
+    text_tokens = []
+    with torch.no_grad():
+        for text in query_array:
+            text_tokens.append(clip.tokenize([text]).detach().to(args.device))
+        
+    text_tokens = torch.cat(text_tokens,dim=0)
+    text_features = clip_model.encode_text(text_tokens)
+    text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+
+    return text_features
+
+def get_type(visual_model):
+    return visual_model.conv1.weight.dtype
+
+def get_clip_model(args):
+    if args.clip_model_type == "B-16":
+        print("Bigger model is being used B-16")
+        clip_model, clip_preprocess = clip.load("ViT-B/16", device=args.device)
+        cond_emb_dim = 512
+    elif args.clip_model_type == "RN50x16":
+        print("Using the RN50x16 model")
+        clip_model, clip_preprocess = clip.load("RN50x16", device=args.device)
+        cond_emb_dim = 768
+    else:
+        clip_model, clip_preprocess = clip.load("ViT-B/32", device=args.device)
+        cond_emb_dim = 512
+    
+    input_resolution = clip_model.visual.input_resolution
+    #train_cond_embs_length = clip_model.train_cond_embs_length
+    vocab_size = clip_model.vocab_size
+    #cond_emb_dim  = clip_model.embed_dim
+    #print("Model parameters:", f"{np.sum([int(np.prod(p.shape)) for p in clip_model.parameters()]):,}")
+    print("cond_emb_dim:", cond_emb_dim)
+    print("Input resolution:", input_resolution)
+    #print("train_cond_embs length:", train_cond_embs_length)
+    print("Vocab size:", vocab_size)
+    args.n_px = input_resolution
+    args.cond_emb_dim = cond_emb_dim
+    return args, clip_model
+
