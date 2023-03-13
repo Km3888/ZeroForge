aeecc2477646c6ef8e84bff9ab39cc8d4762d140
km3888 <km3888@nyu.edu>
2023-03-13 15:46:42 -0400

    Removed tensorboard script

--- tensorboard.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-SINGULARITY_IMAGE=/scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif
-OVERLAY_FILE=/scratch/km3888/singularity_forge/3d.ext3:ro
-
-singularity exec --overlay $OVERLAY_FILE $SINGULARITY_IMAGE /bin/bash -c 'source /ext3/env.sh; tensorboard --logdir ~/general_clip_forge/runs --port=6166'
